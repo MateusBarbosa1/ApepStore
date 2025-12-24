baf6b55c450ec7cddfabb255d8c4d98231347ab3
Mateus Barbosa <pg9779628@gmail.com>
2025-12-24 00:11:13 -0300

    update cart save info

--- controllers/produtosControllers.js
+++ controllers/produtosControllers.js
@@ -1,117 +1,137 @@
 // Biblioteca para decodificar JWT
-const { jwtDecode } = require('jwt-decode');
+const { jwtDecode } = require("jwt-decode");
 
 // Model responsável pelos produtos
-const produtosModel = require('../models/produtosModel');
+const produtosModel = require("../models/produtosModel");
 
 /**
  * Renderiza a página de um produto específico
  */
 module.exports.renderPageProdutoSingle = async function (app, req, res) {
-    // Token do usuário (se estiver logado)
-    const token = req.cookies['token'];
+  // Token do usuário (se estiver logado)
+  const token = req.cookies["token"];
 
-    // ID do produto vindo pela URL
-    const idProduto = req.params.id;
+  // ID do produto vindo pela URL
+  const idProduto = req.params.id;
 
-    // Busca o produto no banco
-    const produto = await produtosModel.getProdutoID(idProduto);
+  // Busca o produto no banco
+  const produto = await produtosModel.getProdutoID(idProduto);
 
-    // Se existir token, tenta identificar o usuário
-    if (token) {
-        const usuariosModel = require('../models/usuariosModel');
+  // Se existir token, tenta identificar o usuário
+  if (token) {
+    const usuariosModel = require("../models/usuariosModel");
 
-        const tokenDecoded = jwtDecode(token);
-        const usuario = await usuariosModel.getUsuarioID(tokenDecoded.id);
+    const tokenDecoded = jwtDecode(token);
+    const usuario = await usuariosModel.getUsuarioID(tokenDecoded.id);
 
-        // Usuário válido
-        if (usuario.length !== 0) {
-            const firstName = usuario[0].nome.split(' ')[0];
-            return res.render('produtos/produto-single', { nome: firstName, produto });
+    // Usuário válido
+    if (usuario.length !== 0) {
+      const firstName = usuario[0].nome.split(" ")[0];
+
+      const carrinhoUsuario = usuario[0].carrinho;
+      let produtoCarrinho = {};
+
+      for (let i = 0; i < carrinhoUsuario.length; i++) {
+        if (carrinhoUsuario[i].id == idProduto) {
+          produtoCarrinho = carrinhoUsuario[i];
         }
+      }
 
-        // Token inválido → remove cookie
-        res.clearCookie('token');
+      return res.render("produtos/produto-single", {
+        nome: firstName,
+        produto,
+        produtoCarrinho: produtoCarrinho,
+        path: req.path,
+      });
     }
 
-    // Usuário não logado
-    res.render('produtos/produto-single', { nome: false, produto });
+    // Token inválido → remove cookie
+    res.clearCookie("token");
+  }
+
+  // Usuário não logado
+  res.render("produtos/produto-single", {
+    nome: false,
+    produto,
+    produtoCarrinho: false,
+    path: req.path,
+  });
 };
 
 /**
  * Renderiza a página de camisetas
  */
 module.exports.renderPageCamisetas = async function (app, req, res) {
-    const token = req.cookies['token'];
+  const token = req.cookies["token"];
 
-    // Busca produtos da categoria camisetas
-    const camisetas = await produtosModel.getProdutosCATEGORIA('camisetas');
+  // Busca produtos da categoria camisetas
+  const camisetas = await produtosModel.getProdutosCATEGORIA("camisetas");
 
-    if (token) {
-        const usuariosModel = require('../models/usuariosModel');
+  if (token) {
+    const usuariosModel = require("../models/usuariosModel");
 
-        const tokenDecoded = jwtDecode(token);
-        const usuario = await usuariosModel.getUsuarioID(tokenDecoded.id);
+    const tokenDecoded = jwtDecode(token);
+    const usuario = await usuariosModel.getUsuarioID(tokenDecoded.id);
 
-        if (usuario.length !== 0) {
-            const firstName = usuario[0].nome.split(' ')[0];
-            return res.render('produtos/camisetas', { nome: firstName, camisetas });
-        }
-
-        res.clearCookie('token');
+    if (usuario.length !== 0) {
+      const firstName = usuario[0].nome.split(" ")[0];
+      return res.render("produtos/camisetas", { nome: firstName, camisetas });
     }
 
-    res.render('produtos/camisetas', { nome: false, camisetas });
+    res.clearCookie("token");
+  }
+
+  res.render("produtos/camisetas", { nome: false, camisetas });
 };
 
 /**
  * Renderiza a página de sapatos
  */
 module.exports.renderPageSapatos = async function (app, req, res) {
-    const token = req.cookies['token'];
+  const token = req.cookies["token"];
 
-    // Busca produtos da categoria sapatos
-    const sapatos = await produtosModel.getProdutosCATEGORIA('sapatos');
+  // Busca produtos da categoria sapatos
+  const sapatos = await produtosModel.getProdutosCATEGORIA("sapatos");
 
-    if (token) {
-        const usuariosModel = require('../models/usuariosModel');
+  if (token) {
+    const usuariosModel = require("../models/usuariosModel");
 
-        const tokenDecoded = jwtDecode(token);
-        const usuario = await usuariosModel.getUsuarioID(tokenDecoded.id);
+    const tokenDecoded = jwtDecode(token);
+    const usuario = await usuariosModel.getUsuarioID(tokenDecoded.id);
 
-        if (usuario.length !== 0) {
-            const firstName = usuario[0].nome.split(' ')[0];
-            return res.render('produtos/sapatos', { nome: firstName, sapatos });
-        }
-
-        res.clearCookie('token');
+    if (usuario.length !== 0) {
+      const firstName = usuario[0].nome.split(" ")[0];
+      return res.render("produtos/sapatos", { nome: firstName, sapatos });
     }
 
-    res.render('produtos/sapatos', { nome: false, sapatos });
+    res.clearCookie("token");
+  }
+
+  res.render("produtos/sapatos", { nome: false, sapatos });
 };
 
 /**
  * Renderiza a página de roupas femininas
  */
 module.exports.renderPageFemininas = async function (app, req, res) {
-    const token = req.cookies['token'];
+  const token = req.cookies["token"];
 
-    // Busca produtos da categoria femininas
-    const femininas = await produtosModel.getProdutosCATEGORIA('femininas');
+  // Busca produtos da categoria femininas
+  const femininas = await produtosModel.getProdutosCATEGORIA("femininas");
 
-    if (token) {
-        const usuariosModel = require('../models/usuariosModel');
+  if (token) {
+    const usuariosModel = require("../models/usuariosModel");
 
-        const tokenDecoded = jwtDecode(token);
-        const usuario = await usuariosModel.getUsuarioID(tokenDecoded.id);
+    const tokenDecoded = jwtDecode(token);
+    const usuario = await usuariosModel.getUsuarioID(tokenDecoded.id);
 
-        if (usuario.length !== 0) {
-            const firstName = usuario[0].nome.split(' ')[0];
-            return res.render('produtos/femininas', { nome: firstName, femininas });
-        }
-
-        res.clearCookie('token');
+    if (usuario.length !== 0) {
+      const firstName = usuario[0].nome.split(" ")[0];
+      return res.render("produtos/femininas", { nome: firstName, femininas });
     }
 
-    res.render('produtos/femininas', { nome: false, femininas });
+    res.clearCookie("token");
+  }
+
+  res.render("produtos/femininas", { nome: false, femininas });
 };
